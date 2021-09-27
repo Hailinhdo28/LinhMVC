5036d30a798ce34b2bced9f68373f1881f8b4155
Linh Do Thi Hai <linhdothihai2830@gmail.com>
2021-09-27 17:29:26 +0700

    code

---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoMVC.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required (ErrorMessage="Title ia required.")]
+        [MaxLength(15)]
+        [MinLength(3)]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
