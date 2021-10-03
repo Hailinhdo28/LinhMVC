64ec7991313851af2eb18dc9b6942fc344e36e47
Linh Do Thi Hai <linhdothihai2830@gmail.com>
2021-10-03 17:25:34 +0700

    123

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -62,7 +62,7 @@ namespace DemoMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating,Email")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -94,7 +94,7 @@ namespace DemoMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating,Email")] Movie movie)
         {
             if (id != movie.Id)
             {
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,20 +1,32 @@
+using System.Xml.Serialization;
+using System.Xml;
 using System;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace DemoMVC.Models
 {
-    public class Movie
-    {
-        [Key]
-        public int Id { get; set; }
-        [Required (ErrorMessage="Title ia required.")]
-        [MaxLength(15)]
-        [MinLength(3)]
-        public string Title { get; set; }
+   public class Movie
+{
+    public int Id { get; set; }
 
-        [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
-        public decimal Price { get; set; }
-    }
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string Title { get; set; }
+
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    public string Genre { get; set; }
+
+    public string Rating { get; set; }
+    //abc@xyz.com
+    [EmailAddress]
+    public string Email { get; set; }
+}
 }
